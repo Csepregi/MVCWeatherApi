4780f51ad5e5e4b82cd1c9ecbc636c4e09948a6c
Gábor Csepregi <csepregigabor02@gmail.com>
2019-08-19 20:58:03 +0200

    weather api

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public Object GetWeatherForecast()
+        {
+            string apikey = "write here your apikey";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apikey;
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
